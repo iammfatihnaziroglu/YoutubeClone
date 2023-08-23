ec0544438870f59914562cc170f55a67120d7a5f
M.Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2023-08-23 20:19:45 +0300

    Youtube clone app (frontend/react.js)
    Youtube clone app project (frontend/react.js)

---
+++ README.md
@@ -0,0 +1,25 @@
+   Hi, This is my first React.js project. 
+   Details here.
+   
+   # Youtube Clone
+ Frontend copy of Youtube Web Application, replicating some features...
+ 
+## Features implemented:
+  1. Home page feed
+  2. Search bar: to search for videos and channels
+  3. Side navigation panel
+  4. Channel detail page
+  5. In app video player with controls
+  6. Recommendation panel: for the videos related to current video
+  7. Video title and statistics
+*************************************
+**API provider site**: ***Rapid API*** - https://rapidapi.com
+
+## References 
+  Build and Deploy a Modern YouTube Clone Application in React JS with Material UI 5 | RapidAPI
+  -- Javascript Mastery --
+  
+  https://youtu.be/FHTbsZEJspU
+
+****************************************************
+
---
+++ src/utils/constants.js
@@ -0,0 +1,43 @@
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'JS Mastery', icon: <CodeIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <DataObjectIcon />, },
+  { name: 'NextJS', icon: <DataObjectIcon />, },
+  { name: 'Music', icon: <LibraryMusicIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <LocalMoviesIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <SportsSoccerIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <MonetizationOnIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UC3dOJFPdcfZ9S42MfU1I1Fg';
+export const demoVideoUrl = '/video/CTwLcjrwU-k';
+export const demoChannelTitle = 'M.Fatih-Konu';
+export const demoVideoTitle = 'Lost Pearl in the Sea';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
